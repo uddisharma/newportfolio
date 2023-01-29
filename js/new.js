ebd185f69431bd795f05923f3a91b335ac094fe0
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-01-29 22:06:34 +0530

    portfolio

---
+++ js/new.js
@@ -0,0 +1,30 @@
+
+function resume() {
+    window.location.href = 'https://drive.google.com/file/d/1rW0JWRmew1BPY_fBLzaVye1lYPKPrC0q/view?usp=share_link'
+}
+function linkedin() {
+    window.location.href = 'https://www.linkedin.com/in/deepak-sharma-3b3a82220/'
+}
+function github() {
+    window.location.href = 'https://github.com/uddisharma'
+}
+function surveymonkey() {
+    window.location.href = 'https://monkey-survey.vercel.app/'
+}
+function neoshop() {
+    window.location.href = 'https://neoshop-bay.vercel.app/'
+}
+function food() {
+    window.location.href = 'https://bassprofrontend.vercel.app/'
+}
+function urbancompany() {
+    window.location.href = 'https://heroic-mochi-61b073.netlify.app'
+}
+function wrike() {
+    window.location.href = 'https://silver-churros-aedf9f.netlify.app'
+}
+function giphy() {
+    window.location.href = 'https://extraordinary-halva-7c95cf.netlify.app/'
+}
+
+
